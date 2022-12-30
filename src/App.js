5dbe1856167bf6a59084de60ed459bd5f62b045a
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2022-12-30 17:27:57 +0500

    Component part-02

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/menu';
+import { DISHES } from './shared/dishes';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Muhammad Tahir Memon</NavbarBrand>  
-        </div>
-      </Navbar>
-      
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       dishes: DISHES
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Muhammad Tahir Memon</NavbarBrand>  
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
