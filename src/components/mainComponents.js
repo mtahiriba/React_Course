e507f75a8e03ee6523e4034070252ca4686c612a
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2022-12-31 18:10:28 +0500

    Single pager website (Contact and home page)

--- src/components/mainComponents.js
+++ src/components/mainComponents.js
@@ -3,6 +3,11 @@ import Menu from './menu';
 import { DISHES } from '../shared/dishes';
 import HeaderComponent from './HeaderComponent';
 import FooterComponent from './FooterComponent';
+import ContactComponent from './contactComponents';
+import { DISHES1 } from '../shared/Dishes1'
+import { PROMOTIONS } from '../shared/Promotions'
+import { LEADERS } from '../shared/Leaders'
+import { COMMENTS } from '../shared/Comments'
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -12,7 +17,11 @@ export default class mainComponents extends Component {
         super(props)
       
         this.state = {
-           dishes: DISHES
+           dishes: DISHES,
+           comments: COMMENTS,
+           leaders: LEADERS,
+           dishes1: DISHES1,
+           promotions: PROMOTIONS
         }
     }
 
@@ -21,7 +30,11 @@ export default class mainComponents extends Component {
 
       const HomeComponent = () => {
         return (
-          <Home/>
+          <Home dish={this.state.dishes1.filter((dish) => dish.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                comment={this.state.comments.filter((comment) => comment.featured)[0]}
+          />
         );
       }
 
@@ -31,6 +44,7 @@ export default class mainComponents extends Component {
         <Switch>
           <Route path='/home' component={HomeComponent}/>
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
+          <Route exact path='/contactus' component={ContactComponent}/>
           <Redirect to='/home'/>
         </Switch>
         <FooterComponent/>
